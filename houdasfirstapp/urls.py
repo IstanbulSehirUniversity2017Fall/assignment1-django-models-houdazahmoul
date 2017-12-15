9b240a8e6e444e719e1ba67b37a9cbb182071e52
houdazahmoul <houdazahmoul@std.sehir.edu.tr>
2017-12-15 20:32:26 +0300

    Updated

--- houdasfirstapp/urls.py
+++ houdasfirstapp/urls.py
@@ -3,9 +3,9 @@ from houdasfirstapp import views
 
  # this looks for the index function in views
 urlpatterns = [
-    url(r'^$', views.index, name='index'), #$ means it went to firstapp by default
+    url(r'^$', views.index), #$ means it went to firstapp by default
 
-    url(r'^(?P<picture_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<picture_id>[0-9]+)/$', views.detail),
 
 ]
 
