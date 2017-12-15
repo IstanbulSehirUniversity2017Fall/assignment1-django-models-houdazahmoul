5e8fb55c89706ac2bba81b0113921989f60337ca
hzahm <33378385+hzahm@users.noreply.github.com>
2017-12-15 20:42:41 +0300

    Add files via upload

---
+++ houdasfirstapp/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from houdasfirstapp import views
+
+ # this looks for the index function in views
+urlpatterns = [
+    url(r'^$', views.index), #$ means it went to firstapp by default
+
+    url(r'^(?P<picture_id>[0-9]+)/$', views.detail),
+
+]
+
